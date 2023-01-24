0afd8f0e916bc8e0356db149e0c61459c1c8e578
MarceloMosl <marcelomosl@icloud.com>
2023-01-24 20:24:42 -0300

    Deploy Banco de DADOS e Back-end

--- src/app.js
+++ src/app.js
@@ -9,7 +9,16 @@ app.use(json());
 
 app.get("/teste", async (req, res) => {
   const teste = await db.collection("users").find({}).toArray();
-  res.send({ reposta: teste });
+  res.send({ teste });
+});
+
+app.post("/teste", async (req, res) => {
+  try {
+    await db.collection("users").insertOne(req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(PORT, () => console.log("Server ON"));
