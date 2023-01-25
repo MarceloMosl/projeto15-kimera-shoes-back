5dc1bfa91c9301bf7d28d5c1fe00f75e0e59551d
MarceloMosl <marcelomosl@icloud.com>
2023-01-25 17:24:22 -0300

    feat: Get /produtos e /produtos?categria

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
 import express, { json } from "express";
 import cors from "cors";
-import db from "./config/dataBase.js";
+import prodRouter from "./routes/prodRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 app.use(cors());
 app.use(json());
 
-app.get("/teste", async (req, res) => {
-  const teste = await db.collection("users").find({}).toArray();
-  res.send({ teste });
-});
-
-app.post("/teste", async (req, res) => {
-  try {
-    await db.collection("users").insertOne(req.body);
-    res.sendStatus(200);
-  } catch (error) {
-    res.send(error);
-  }
-});
+app.use([prodRouter]);
 
 app.listen(PORT, () => console.log("Server ON"));
