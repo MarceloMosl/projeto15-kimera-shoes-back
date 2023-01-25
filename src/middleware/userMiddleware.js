b8bae2048c2147c322db7eab04bb8228190dcaf6
Gisele <gisele_s@hotmail.com>
2023-01-25 16:26:42 -0300

    middleware

---
+++ src/middleware/userMiddleware.js
@@ -0,0 +1,24 @@
+import db from "../config/dataBase";
+
+export async function userMiddleware (req, res, next){
+    const {authorization} = req.headers;
+    const token = authorization?.replace('Bearer', '').trim()
+
+    if(!token) return res.status(401).send("token inexistente")
+
+    try {
+        const session = await db.collection("sessions").findOne({token});
+        if(!session) return res.status(401).send("Não existe essa sessão")
+
+        const user = await db.collection("users").findOne({_id: session.userId})
+        if(!user) return res.status(401).send("Usuário não cadastrado")
+
+        res.locals.user = user
+        next()
+    } catch (error) {
+        console.log("erro no middleware")
+        console.log(error)
+        return res.sendStatus(500)
+    }
+
+}
