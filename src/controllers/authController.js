07b46d23352ffcd83076a1d24dbbc161ff0d8f7c
Gisele <gisele_s@hotmail.com>
2023-01-25 17:23:38 -0300

    função login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,31 @@ import bcrypt from "bcrypt"
 import {v4 as uuid} from "uuid"
 
 export async function login(req, res){
+    try {
+        const user = req.body
+        const validate = authLoginSchema.validate(user)
 
+        if(validate.error){
+            return res.status(422).send("Preencha todos os campos")
+        }
+
+        const checkUser = await db.collection("users").findOne({email: user.email})
+        if(!checkUser){
+            return res.status(422).send("email não cadastrado")
+        }
+
+        const passwordBcrypt = bcrypt.compareSync(user.password, checkUser.password)
+        if(passwordBcrypt){
+            const token = uuid()
+            await db.collection("sessions").insertOne({token, userID: verificaUser._id})
+            return res.status(200).send({token, name: checkUser.name})
+        }
+        res.status(200).send("Login realizado com sucesso")
+
+    } catch (error) {
+        console.error("Erro ao cadastrar usuário")
+        res.status(500).send(error)
+    }
 }
 
 export async function cadastro (req, res){ 
