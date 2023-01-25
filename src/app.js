6ff8c5a2621cf360ae017e6912daa56b3dad0429
Gisele <gisele_s@hotmail.com>
2023-01-25 18:17:54 -0300

    import authroutes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import prodRouter from "./routes/prodRoutes.js";
+import authRoutes from "./routes/authRoutes.js"
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(json());
 
 app.use([prodRouter]);
+app.use(authRoutes);
 
 app.listen(PORT, () => console.log("Server ON"));
