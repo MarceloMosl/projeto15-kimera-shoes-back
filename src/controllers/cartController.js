294615380999ca1c335358100a36011d7ce7d1e0
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-27 19:20:12 -0300

    feat: created a function that returns all products on cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,6 +1,33 @@
 import { ObjectId } from "mongodb";
 import db from "../config/dataBase.js";
 
+export async function getProductsOnCart(req, res){
+    if (!req.headers.authentication) return res.status(401).send("Token inválido");
+    const auth = req.headers.authentication.replace("Bearer", "");
+
+    try {
+        const {userId} = await db.collection("sessions").findOne({token: auth});
+        if (!userId) return res.status(404).send("Usuário não encontrado");
+
+        const result = [];
+
+        const productsOnCart = await db.collection("cart").find({userId}).toArray();
+
+        for (const product of productsOnCart) {
+            const productData = await db.collection("products").findOne({_id: product.productId});
+            result.push({
+                productId: (productData._id).toString(),
+                quant: product.quant,
+            });
+        }
+
+        res.send(result);
+    } catch (error) {
+        console.log(`getProductsOnCart Function Error: ${error}`);
+        res.sendStatus(500);
+    }
+}
+
 export async function addProductOnCart(req, res) {
     const {product_id, quant} = req.body;
     let auth = req.headers.authentication;
