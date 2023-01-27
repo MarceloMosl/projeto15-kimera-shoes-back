24cd1e3b0ed4481db08af9040b70336664bec9fb
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-27 19:21:22 -0300

    feat: added a get route that return all products

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { addProductOnCart, alterQuantitiesOnCart, deleteProductOnCart } from "../controllers/cartController.js";
+import { 
+    addProductOnCart,
+    alterQuantitiesOnCart,
+    deleteProductOnCart,
+    getProductsOnCart
+} from "../controllers/cartController.js";
 
 const router = Router();
 
+router.get("/carrinho", getProductsOnCart);
 router.post("/carrinho", addProductOnCart);
 router.put("/carrinho", alterQuantitiesOnCart);
 router.delete("/carrinho", deleteProductOnCart);
