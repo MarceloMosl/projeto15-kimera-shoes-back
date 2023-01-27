d5c4e94fe83be0010a89e886bf9458d1e5457933
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-27 16:48:21 -0300

    feat: created add product on cart function

---
+++ src/controllers/cartController.js
@@ -0,0 +1,26 @@
+import { ObjectId } from "mongodb";
+import db from "../config/dataBase.js";
+
+export async function addProductonCart(req, res) {
+    const {product_id, quant} = req.body;
+    let auth = req.headers.authentication;
+    auth = auth.replace("Bearer ", "");
+
+    if (!auth) return res.status(401).send("Usuário não autorizado");
+
+    try {
+        const userId = await db.collection("sessions").findOne({token: auth});
+        if (!userId) return res.status(404).send("Usuário não autorizado");
+
+        const product = await db.collection("products").findOne({_id: ObjectId(product_id)}, { quant: 1 });
+        if(!product) return res.status(404).send("Produto não encontrado");
+
+        if (quant > product.quant) return res.status(409);
+
+        await db.collection("cart").insertOne({userId: userId.userId, productId: product._id, quant});
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(`getCart Function Error: ${error}`);
+        res.sendStatus(500);
+    }
+}
