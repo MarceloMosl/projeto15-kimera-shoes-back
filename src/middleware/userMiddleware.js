283b4424834279aadba5fbda6514e73031d2f61e
MarceloMosl <marcelomosl@icloud.com>
2023-01-28 13:15:33 -0300

    Fix: hotfix import

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -1,24 +1,23 @@
-import db from "../config/dataBase";
+import db from "../config/dataBase.js";
 
-export async function userMiddleware (req, res, next){
-    const {authorization} = req.headers;
-    const token = authorization?.replace('Bearer', '').trim()
+export async function userMiddleware(req, res, next) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer", "").trim();
 
-    if(!token) return res.status(401).send("token inexistente")
+  if (!token) return res.status(401).send("token inexistente");
 
-    try {
-        const session = await db.collection("sessions").findOne({token});
-        if(!session) return res.status(401).send("Não existe essa sessão")
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) return res.status(401).send("Não existe essa sessão");
 
-        const user = await db.collection("users").findOne({_id: session.userId})
-        if(!user) return res.status(401).send("Usuário não cadastrado")
+    const user = await db.collection("users").findOne({ _id: session.userId });
+    if (!user) return res.status(401).send("Usuário não cadastrado");
 
-        res.locals.user = user
-        next()
-    } catch (error) {
-        console.log("erro no middleware")
-        console.log(error)
-        return res.sendStatus(500)
-    }
-
+    res.locals.user = user;
+    next();
+  } catch (error) {
+    console.log("erro no middleware");
+    console.log(error);
+    return res.sendStatus(500);
+  }
 }
